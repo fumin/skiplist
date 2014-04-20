88eabbb44a79676f6839dfb0c0d6498458eec655
awaw fumin <awawfumin@gmail.com>
2014-04-20 20:11:06 +0800

    add period

--- skiplist.go
+++ skiplist.go
@@ -176,7 +176,7 @@ func (z *Skiplist) Rem(ordered Ordered) bool {
   return false
 }
 
-// Range returns elements whose rank is between start and stop
+// Range returns elements whose rank is between start and stop.
 // Both arguments, start and stop are inclusive, and are 0 based.
 func (z *Skiplist) Range(start, stop int) (reply []Ordered) {
   if start > stop || start >= z.length {
