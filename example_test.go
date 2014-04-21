c4f8b57f7d3a2e5a2fcd28eeae453cdca31c7f33
awaw fumin <awawfumin@gmail.com>
2014-04-22 03:01:47 +0800

    more examples

--- example_test.go
+++ example_test.go
@@ -1,25 +1,19 @@
-package skiplist_test
+package skiplist
 
 import (
+	"fmt"
 	"math/rand"
-
-	"github.com/fumin/skiplist"
 )
-
-type Int int
-
-func (i Int) Less(j skiplist.Ordered) bool {
-	return i < j.(Int)
-}
 
 func ExampleSkiplist_Insert() {
 	rand.Seed(1)
 
-	z := skiplist.New()
+	z := New()
 	for i := 0; i < 20; i++ {
 		z.Insert(Int(i))
 	}
 	z.PrintDebug()
+
 	// Output:
 	// length: 20, level: 5
 	// head____________ 6________________________
@@ -27,4 +21,18 @@ func ExampleSkiplist_Insert() {
 	// head____________ 6__ 8______12____15______
 	// head__________ 5 6 7 8____1112____15__17__
 	// head 0 1 2 3 4 5 6 7 8 910111213141516171819
+}
+
+func ExampleSkiplist_SampleInRange() {
+	rand.Seed(1)
+
+	z := New()
+	for i := 0; i < 20; i++ {
+		z.Insert(Int(i))
+	}
+	sampled := z.SampleInRange(RangeSpec{Min: Int(4), Max: Int(17)}, 5)
+	fmt.Println(sampled)
+
+	// Output:
+	// [5 8 10 13 14]
 }
